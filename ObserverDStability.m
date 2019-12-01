22e4265ce285118b2315b0cca66eb6dd4546376b
aramani3 <57736023+aramani3@users.noreply.github.com>
2019-12-01 11:48:44 -0700

    Update ObserverDStability.m

--- ObserverDStability.m
+++ ObserverDStability.m
@@ -1,4 +1,4 @@
-%MATLAB-YALMIP example implementation of the continuous-time D-stability controller.
+%MATLAB-YALMIP example implementation of the continuous-time D-stability observer.
 
 A = [-1.3410 0.9933 0 -0.1689 -0.2518
     43.2230 -0.8693 0 -17.2510 -1.5766
@@ -9,7 +9,7 @@ B = [0 0
     0 0
     0 0
     20 0
-    0 20];
+    0 20];%parameters used for this example
 [~,n]=size(A);
 [c1,c2]=size(B);
 P4=sdpvar(n,n);Z2=sdpvar(c1,c2);
@@ -18,7 +18,7 @@ F2=[P4>=1e-5*eye(n)];
 F2=[F2,[(-r*P4),(P4*A+Z2*C)';(P4*A+Z2*C),(-r*P4)]<=1e-5*eye(2*n)];
 F2=[F2,(P4*A+Z2*C)'+P4*A+Z2*C+2*alpha*P4<=1e-5*eye(n)];
 F2=[F2,[((P4*A+Z2*C)'+P4*A+Z2*C),c*((P4*A+Z2*C)'-(P4*A+Z2*C));c*((P4*A+Z2*C)-(P4*A+Z2*C)'),((P4*A+Z2*C)'+P4*A+Z2*C)]<=1e-5*eye(2*n)];
-optimize(F2,[],options);
+optimize(F2,[],options);%execute the given problem
 Z2=value(Z2);P4=value(P4);
 L=inv(P4)*Z2%display the resulting L matrix
 ApLC_eig=eig(A+L*C)%display the resulting eigenvalues of A+LC
