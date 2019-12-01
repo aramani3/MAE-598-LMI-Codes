c7ca44ade7bc23d8ae0c9bcd3cf806f0048661b2
aramani3 <57736023+aramani3@users.noreply.github.com>
2019-12-01 11:47:30 -0700

    Update DelayIndependentTimeDelay.m

--- DelayIndependentTimeDelay.m
+++ DelayIndependentTimeDelay.m
@@ -1,4 +1,4 @@
-%MATLAB-YALMIP implementation of the Delay Independent Time-Delayed System
+%MATLAB-YALMIP example implementation of the Delay Independent Time-Delayed System
 clc;clear all;close all;
 A=[-1 0 1
    0 2 -1
@@ -6,15 +6,15 @@ A=[-1 0 1
 Ad=[1 0 1
     2 1 1
     0 0 -1];
-B=[1 1;1 2;0 1];
+B=[1 1;1 2;0 1];%parameters used for this example
 X=sdpvar(size(A,1));W=sdpvar(size(B,2),size(B,1),'full');
-Y=sdpvar(size(A,1));
+Y=sdpvar(size(A,1));%setting up variables to be solved in YALMIP
 F=[X*A'+A*X+B*W+W'*B'+Y     Ad*X;
    X*Ad'                    -Y];
 Constraints=[X>=1e-5*eye(size(A,1));Y>=1e-5*eye(size(A,1))];
 Constraints=[Constraints;F<=0];
-optimize(Constraints);
+optimize(Constraints);%execute the given problem
 W=value(W)
 X=value(X)
 Y=value(Y)
-K=W*inv(X)
+K=W*inv(X)%display values determined by the solver
