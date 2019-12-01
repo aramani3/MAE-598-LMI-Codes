c14f00665a8193149ba96722f5741cd05a93aaaa
aramani3 <57736023+aramani3@users.noreply.github.com>
2019-12-01 11:45:11 -0700

    Update DelayDependentTimeDelay.m

--- DelayDependentTimeDelay.m
+++ DelayDependentTimeDelay.m
@@ -1,4 +1,4 @@
-%MATLAB-YALMIP implementation of the Delay Dependent Time-Delayed System
+%MATLAB-YALMIP example implementation of the Delay Dependent Time-Delayed System
 clc;clear all;close all;
 A=[-1 0 1
    0 2 -1
@@ -6,17 +6,17 @@ A=[-1 0 1
 Ad=[1 0 1
     2 1 1
     0 0 -1];
-B=[1 1;1 2;0 1];dbar=0.1;
+B=[1 1;1 2;0 1];dbar=0.1;%parameters used for this example
 sdpvar beta;
-X=sdpvar(size(A,1));W=sdpvar(size(B,2),size(B,1),'full');
-Phi_XW=X*(A+Ad)'+(A+Ad)*X+B*W+W'*B'+dbar*Ad*Ad';
+X=sdpvar(size(A,1));W=sdpvar(size(B,2),size(B,1),'full');%setting up variables to be solved in YALMIP
+Phi_XW=X*(A+Ad)'+(A+Ad)*X+B*W+W'*B'+dbar*Ad*Ad';%Phi(X,W) function
 F=[Phi_XW       dbar*(X*A'+W'*B')       dbar*X*(Ad');
    dbar*(A*X+B*W) -dbar*beta*eye(size(A,1))   zeros(3)
    dbar*Ad*X    zeros(3)                -dbar*(1-beta)*eye(3)];
 Constraints=[X>=1e-5*eye(size(A,1));0<=beta<=1];
 Constraints=[Constraints;F<=0];
-optimize(Constraints);
+optimize(Constraints);%execute the given problem
 W=value(W)
 X=value(X)
 beta=value(beta)%value is between 0 and 1
-K=W*inv(X)
+K=W*inv(X)%display values determined by the solver
