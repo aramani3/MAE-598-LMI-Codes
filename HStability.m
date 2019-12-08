26537261f1c89429eadf8652b2ecbc7c9a5b2c07
aramani3 <57736023+aramani3@users.noreply.github.com>
2019-12-07 20:14:09 -0700

    Update HStability.m

--- HStability.m
+++ HStability.m
@@ -10,9 +10,9 @@ end
 B=[1 zeros(1,19)]';C=[zeros(1,19) 1];%set up matrices for this example
 P=sdpvar(20);W=sdpvar(size(B,2) ,size(B,1),'full');
 alpha=1;beta=3;
-K=value(W)*inv(value(P))%display resulting controller
 
 M=A*P+P*A'+B*W+W'*B'+2*alpha*P;
 M2=-(A*P+P*A'+B*W+W'*B')-2*beta*P;
 Constraints=[P>=1e-5*eye(size(A,1));M<=0;M2<=0];%setup constraints
 optimize(Constraints);%execute the code
+K=value(W)*inv(value(P))%display resulting controller
